464d68b5ecf9d9ef1278d5d601338dc30fb9a841
Remych04 <remych04@gmail.com>
2020-01-08 22:09:25 +0300

    nav by bottom navigation

--- app/src/main/java/com/remych04/dtf/feature/category/view/CategoryFragment.kt
+++ app/src/main/java/com/remych04/dtf/feature/category/view/CategoryFragment.kt
@@ -1,5 +1,9 @@
 package com.remych04.dtf.feature.category.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.remych04.dtf.databinding.CategoryFragmentBinding
@@ -15,4 +19,11 @@ class CategoryFragment : DaggerFragment() {
 
     private lateinit var binding: CategoryFragmentBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = CategoryFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
